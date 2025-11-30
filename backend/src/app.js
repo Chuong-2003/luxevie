17117d058f3abe7ac71e80ac6335754c738348cc
duy10102003 <duya1k20nl4@gmail.com>
2025-11-30 22:39:29 +0700

    Final version

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,14 +21,9 @@ const allowed = [
 // middleware CORS
 app.use(
   cors({
-    origin(origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowed.includes(origin)) return callback(null, true);
-      return callback(new Error("Not allowed by CORS"));
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    origin: true, // cho phép tất cả origin
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // nếu dùng cookie
   })
 );
 
